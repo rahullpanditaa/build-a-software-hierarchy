a28db06f1a1887cd3a659de53d0b09295c929e58
Rahul Pandita <rpandita1999@outlook.com>
2025-01-16 14:00:05 +0530

    Created fields that hold pre-defined base addresses of certain memory segments and a stack pointer

--- VMTranslator/src/CodeWriter.java
+++ VMTranslator/src/CodeWriter.java
@@ -1,2 +1,24 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+
 public class CodeWriter {
+    // base addresses for memory segments in RAM
+    // LCL, ARG, THIS, THAT, POINTER are allocated segment dynamically
+    private static final int TEMP_SEGMENT_BASE = 5;
+    private static final int GENERAL_REGISTERS_BASE = 13;
+    private static final int STATIC_VARIABLES_SEGMENT_BASE = 16;
+    private static final int STACK_BASE = 256;
+    private static int STACK_POINTER = 256;
+    BufferedWriter writer;
+
+    public CodeWriter(String asmFile) {
+        try {
+            writer = new BufferedWriter(new FileWriter(asmFile));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
